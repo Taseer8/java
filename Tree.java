c5818f2eec3747246714b665779d04effd7ff552
Taseer8 <taseer08.xworkz@gmail.com>
2024-08-06 16:58:34 +0530

    Tree file

---
+++ Tree.java
@@ -0,0 +1,18 @@
+public class Tree{
+	public static void main(String[] args){
+		int count=0;
+		System.out.println(args.length);
+		for(int i = 0; i<args.length; i++ ){
+			count++;
+			//System.out.println(args[i]);
+			if(args[i].equals("mango")){
+				
+				//System.out.println(count);
+			}
+			
+		
+		}
+		System.out.println(count);
+	}
+
+}
